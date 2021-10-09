0ccb1775481215c79243e61949c8cb88f39cab5a
lazaroofarrill <lazaroofarrill@gmail.com>
2021-10-09 16:05:17 -0400

    creating notebook

--- tarea.py
+++ tarea.py
@@ -1,4 +1,7 @@
 import numpy as np
+from matplotlib import pyplot as plt
+
+fm = 2000
 
 channels = []
 for i in range(1, 6):
@@ -6,5 +9,13 @@ for i in range(1, 6):
     c = np.fromfile(fid, np.int16)
     channels.append(c)
 
+for idx in range(len(channels)):
+    channels[idx] = list(filter(lambda x: x != 0, channels[idx]))
+
 for i in channels:
-    print(i)
+    plt.plot(range(0, len(i)), i)
+
+# plt.plot(range(0, len(channels[4])), channels[4])
+plt.xlabel('Time (ms)')
+plt.ylabel('Amp (mV)')
+plt.show()
